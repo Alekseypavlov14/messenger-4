c575697bc3b8d5adcbecd140b1180d4538d889c9
Alekseypavlov14 <aleshapavlov9@gmail.com>
2023-02-09 11:15:27 +0200

    added container to header

--- src/widgets/Header/components/Header/Header.tsx
+++ src/widgets/Header/components/Header/Header.tsx
@@ -2,14 +2,17 @@ import { FC } from 'react'
 import { SearchIcon } from '@features/SearchUsers'
 import { Title } from '../Title/Title'
 import styles from './Header.module.css'
+import { Container } from 'standard-ui'
 
 interface HeaderProps {}
 
 export const Header: FC<HeaderProps> = () => {
   return (
     <div className={styles.Header}>
-      <Title />
-      <SearchIcon />
+      <Container className={styles.HeaderContainer}>
+        <Title />
+        <SearchIcon />
+      </Container>
     </div>
   )
 }
